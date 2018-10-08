819d2b63f241f06bee738a9a67bb919ed51446a6
“Emilio” <emilio.jeldes@gmail.com>
2018-10-07 22:34:27 -0300

    added bash script to make a general commands.txt

---
+++ CentOS/get_commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat ./Commands/add_partition.txt > commands.txt
+cat ./Commands/format_partition.txt >> commands.txt
+cat ./Commands/mount_point.txt >> commands.txt
+cat ./Commands/network.txt >> commands.txt
+cat ./Commands/system_info.txt >> commands.txt
+cat ./Commands/miscellaneous.txt >> commands.txt

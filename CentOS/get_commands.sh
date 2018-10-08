51bcb8af108009bb6d7f4de9b58f3e301d27f000
“Emilio” <emilio.jeldes@gmail.com>
2018-10-08 03:36:41 -0300

    swap memory added to script

--- CentOS/get_commands.sh
+++ CentOS/get_commands.sh
@@ -3,6 +3,7 @@
 cat ./Commands/add_partition.txt > commands.txt
 cat ./Commands/format_partition.txt >> commands.txt
 cat ./Commands/mount_point.txt >> commands.txt
+cat ./Commands/swap_format.txt >> commands.txt
 cat ./Commands/network.txt >> commands.txt
 cat ./Commands/system_info.txt >> commands.txt
 cat ./Commands/miscellaneous.txt >> commands.txt
